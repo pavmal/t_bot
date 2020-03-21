ac7898ae63034e33c976f5fdc6b49269e3c50885
pavmal <pavmalin@mail.ru>
2020-03-21 21:41:48 +0300

    ver_01

---
+++ t_millions.py
@@ -0,0 +1,41 @@
+import telebot
+token = '1064357091:AAHg0oamqbvoDmRL7Nu7gGEZvi_W7aeHSjg'
+
+bot = telebot.TeleBot(token)
+result = {'victories': 0, 'defeats': 0}
+#telebot.apihelper.proxy = {'https': 'socks5://stepik.akentev.com:1080'}
+
+@bot.message_handler(fanc=lambda message: True)
+def echo(message):
+    bot.reply_to(message, message.text)
+
+"""
+@bot.message_handler(fanc=lambda message: True)
+def echo(message):
+    if message.text == '/start':
+        bot.reply_to(message,'Это бот-игра в "Кто хочет стать миллионером"')
+    elif message.text == 'Привет':
+        bot.reply_to(message, 'Ну привет! ' + message.from_user.user_name)
+    elif message.text == 'Спроси меня вопрос':
+        bot.reply_to(message, 'Какую площадь имеет клетка стандартной школьной тетрадки: 0,25; 1,00; 0,5; 1,25')
+    elif message.text == '0,25':
+        bot.reply_to(message, 'Правильно!')
+        result['victories'] += 1
+    elif message.text == '1,0':
+        bot.reply_to(message, 'Неправильно :(')
+        result['defeats'] += 1
+    elif message.text == '1,25':
+        bot.reply_to(message, 'Неправильно :(')
+        result['defeats'] += 1
+    elif message.text == '0,5':
+        bot.reply_to(message, 'Неправильно :(')
+        result['defeats'] += 1
+    elif message.text == 'Покажи счёт':
+        bot.reply_to(message, 'Текущий счет: Побед - '  + str(result['victories']) + \
+                                            ' Поражений - ' + str(result['defeats']))
+    else:
+        bot.reply_to(message, 'Я тебя не понял :(')
+#    else:
+#        bot.reply_to(message, message.text)
+"""
+bot.polling()
